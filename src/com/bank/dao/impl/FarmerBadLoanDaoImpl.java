0491366b77ae7df988fa6d7ecc6b8f31e4e92f77
techpicker <1023144975@qq.com>
2015-04-18 00:06:04 +0800

    farmer

--- src/com/bank/dao/impl/FarmerBadLoanDaoImpl.java
+++ src/com/bank/dao/impl/FarmerBadLoanDaoImpl.java
@@ -2,12 +2,12 @@ package com.bank.dao.impl;
 
 import org.springframework.stereotype.Repository;
 
-import com.bank.beans.FarmerBadloan;
+import com.bank.beans.FarmerBadLoan;
 import com.bank.dao.IFarmerBadLoanDao;
 import com.common.dao.impl.GenericMyBatisDAOSupport;
 
 @Repository("farmerBadLoanDao")
-public class FarmerBadLoanDaoImpl extends GenericMyBatisDAOSupport<FarmerBadloan, Long>
+public class FarmerBadLoanDaoImpl extends GenericMyBatisDAOSupport<FarmerBadLoan, Long>
 	implements IFarmerBadLoanDao {
 
 }
