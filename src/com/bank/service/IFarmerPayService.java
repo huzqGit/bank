f032139f28d230deb27c33bdadd574426c34fb1f
techpicker <1023144975@qq.com>
2015-05-09 23:11:40 +0800

    最新服务类更新

--- src/com/bank/service/IFarmerPayService.java
+++ src/com/bank/service/IFarmerPayService.java
@@ -1,9 +1,28 @@
 package com.bank.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.bank.beans.Farmer;
+import com.bank.beans.FarmerIncome;
 import com.bank.beans.FarmerPay;
+import com.common.exception.CreateException;
+import com.common.exception.DAOException;
+import com.common.exception.DataNotFoundException;
+import com.common.exception.UpdateException;
 import com.common.service.GenericService;
 
 public interface IFarmerPayService extends
 		GenericService<FarmerPay, Long> {
+	
+	public void saveBalance(Farmer farmer,FarmerPay balance,List<FarmerIncome> incomes) 
+			throws DAOException, DataNotFoundException,
+			CreateException,UpdateException;
+	
+	public List<FarmerPay> loadPayByDateAndFarmer(Long farmerId,String year);
+	
+	public List<FarmerIncome> loadTotalIncome(Long balanceId);
+	//查找用户指定年份的收支信息
+	public List<FarmerPay> findByFarmerAndYear();
 
 }
---
+++ src/com/bank/service/INewsService.java
@@ -0,0 +1,8 @@
+package com.bank.service;
+
+import com.bank.beans.News;
+import com.common.service.GenericService;
+
+public interface INewsService extends GenericService<News, Long> {
+
+}
