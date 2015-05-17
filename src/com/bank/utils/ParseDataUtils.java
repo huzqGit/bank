e3f497d2e78efca05b1d889e58a73730f460daf6
wind0727 <82651730@qq.com>
2015-05-17 18:48:25 +0800

    在finally关闭流

--- src/com/bank/utils/ParseDataUtils.java
+++ src/com/bank/utils/ParseDataUtils.java
@@ -34,12 +34,11 @@ import org.slf4j.LoggerFactory;
 
 public class ParseDataUtils {
 	private static Logger log = LoggerFactory.getLogger(ParseDataUtils.class);
-	public static final String OFFICE_EXCEL_2003_POSTFIX = "xls";
-	public static final String OFFICE_EXCEL_2010_POSTFIX = "xlsx";
-	public static final String EMPTY = "";
-	public static final String POINT = ".";
-	public static final String NOT_EXCEL_FILE = " : Not the Excel file!";
-	public static final String PROCESSING = "Processing...";
+	private static final String OFFICE_EXCEL_2003_POSTFIX = "xls";
+	private static final String OFFICE_EXCEL_2010_POSTFIX = "xlsx";
+	private static final String EMPTY = "";
+	private static final String POINT = ".";
+	private static final String NOT_EXCEL_FILE = " : Not the Excel file!";
 
 	public static String[][] getExeclData(String path, int ignoreRows)
 			throws FileNotFoundException, IOException {
@@ -295,167 +294,174 @@ public class ParseDataUtils {
 
 		int rowSize = 0;
 
-		BufferedInputStream in = new BufferedInputStream(inputStream);
+		String[][] returnArray = null;
+		BufferedInputStream in = null;
+		try {
+			in = new BufferedInputStream(inputStream);
 
-		// 打开HSSFWorkbook
-
-		POIFSFileSystem fs = new POIFSFileSystem(in);
-
-		HSSFWorkbook wb = new HSSFWorkbook(fs);
-
-		HSSFCell cell = null;
-
-		for (int sheetIndex = 0; sheetIndex < wb.getNumberOfSheets(); sheetIndex++) {
-
-			HSSFSheet st = wb.getSheetAt(sheetIndex);
-
-			// 第一行为标题，不取
-
-			for (int rowIndex = ignoreRows; rowIndex <= st.getLastRowNum(); rowIndex++) {
-
-				HSSFRow row = st.getRow(rowIndex);
-
-				if (row == null) {
-
-					continue;
-
-				}
-
-				int tempRowSize = row.getLastCellNum() + 1;
-
-				if (tempRowSize > rowSize) {
-
-					rowSize = tempRowSize;
-
-				}
-
-				String[] values = new String[rowSize];
-
-				Arrays.fill(values, "");
-
-				boolean hasValue = false;
-
-				for (short columnIndex = 0; columnIndex <= row.getLastCellNum(); columnIndex++) {
-
-					String value = "";
-
-					cell = row.getCell(columnIndex);
-
-					if (cell != null) {
-
-						// 注意：一定要设成这个，否则可能会出现乱码
-
-						// cell.setEncoding(HSSFCell.ENCODING_UTF_16);
-
-						switch (cell.getCellType()) {
-
-						case HSSFCell.CELL_TYPE_STRING:
-
-							value = cell.getStringCellValue();
-
-							break;
-
-						case HSSFCell.CELL_TYPE_NUMERIC:
-
-							if (HSSFDateUtil.isCellDateFormatted(cell)) {
-
-								Date date = cell.getDateCellValue();
-
-								if (date != null) {
-
-									value = new SimpleDateFormat("yyyy-MM-dd")
-
-									.format(date);
-
-								} else {
-
-									value = "";
-
-								}
-
-							} else {
-
-								value = new DecimalFormat("0").format(cell
-
-								.getNumericCellValue());
-
-							}
-
-							break;
-
-						case HSSFCell.CELL_TYPE_FORMULA:
-
-							// 导入时如果为公式生成的数据则无值
-
-							if (!cell.getStringCellValue().equals("")) {
-
+			// 打开HSSFWorkbook
+	
+			POIFSFileSystem fs = new POIFSFileSystem(in);
+	
+			HSSFWorkbook wb = new HSSFWorkbook(fs);
+	
+			HSSFCell cell = null;
+	
+			for (int sheetIndex = 0; sheetIndex < wb.getNumberOfSheets(); sheetIndex++) {
+	
+				HSSFSheet st = wb.getSheetAt(sheetIndex);
+	
+				// 第一行为标题，不取
+	
+				for (int rowIndex = ignoreRows; rowIndex <= st.getLastRowNum(); rowIndex++) {
+	
+					HSSFRow row = st.getRow(rowIndex);
+	
+					if (row == null) {
+	
+						continue;
+	
+					}
+	
+					int tempRowSize = row.getLastCellNum() + 1;
+	
+					if (tempRowSize > rowSize) {
+	
+						rowSize = tempRowSize;
+	
+					}
+	
+					String[] values = new String[rowSize];
+	
+					Arrays.fill(values, "");
+	
+					boolean hasValue = false;
+	
+					for (short columnIndex = 0; columnIndex <= row.getLastCellNum(); columnIndex++) {
+	
+						String value = "";
+	
+						cell = row.getCell(columnIndex);
+	
+						if (cell != null) {
+	
+							// 注意：一定要设成这个，否则可能会出现乱码
+	
+							// cell.setEncoding(HSSFCell.ENCODING_UTF_16);
+	
+							switch (cell.getCellType()) {
+	
+							case HSSFCell.CELL_TYPE_STRING:
+	
 								value = cell.getStringCellValue();
-
-							} else {
-
-								value = cell.getNumericCellValue() + "";
-
+	
+								break;
+	
+							case HSSFCell.CELL_TYPE_NUMERIC:
+	
+								if (HSSFDateUtil.isCellDateFormatted(cell)) {
+	
+									Date date = cell.getDateCellValue();
+	
+									if (date != null) {
+	
+										value = new SimpleDateFormat("yyyy-MM-dd")
+	
+										.format(date);
+	
+									} else {
+	
+										value = "";
+	
+									}
+	
+								} else {
+	
+									value = new DecimalFormat("0").format(cell
+	
+									.getNumericCellValue());
+	
+								}
+	
+								break;
+	
+							case HSSFCell.CELL_TYPE_FORMULA:
+	
+								// 导入时如果为公式生成的数据则无值
+	
+								if (!cell.getStringCellValue().equals("")) {
+	
+									value = cell.getStringCellValue();
+	
+								} else {
+	
+									value = cell.getNumericCellValue() + "";
+	
+								}
+	
+								break;
+	
+							case HSSFCell.CELL_TYPE_BLANK:
+	
+								break;
+	
+							case HSSFCell.CELL_TYPE_ERROR:
+	
+								value = "";
+	
+								break;
+	
+							case HSSFCell.CELL_TYPE_BOOLEAN:
+	
+								value = (cell.getBooleanCellValue() == true ? "Y"
+	
+								: "N");
+	
+								break;
+	
+							default:
+	
+								value = "";
+	
 							}
-
-							break;
-
-						case HSSFCell.CELL_TYPE_BLANK:
-
-							break;
-
-						case HSSFCell.CELL_TYPE_ERROR:
-
-							value = "";
-
-							break;
-
-						case HSSFCell.CELL_TYPE_BOOLEAN:
-
-							value = (cell.getBooleanCellValue() == true ? "Y"
-
-							: "N");
-
-							break;
-
-						default:
-
-							value = "";
-
+	
 						}
-
+	
+						if (columnIndex == 0 && value.trim().equals("")) {
+	
+							break;
+	
+						}
+	
+						values[columnIndex] = rightTrim(value);
+	
+						hasValue = true;
+	
 					}
-
-					if (columnIndex == 0 && value.trim().equals("")) {
-
-						break;
-
+	
+					if (hasValue) {
+	
+						result.add(values);
+	
 					}
-
-					values[columnIndex] = rightTrim(value);
-
-					hasValue = true;
-
+	
 				}
-
-				if (hasValue) {
-
-					result.add(values);
-
-				}
-
+	
 			}
-
-		}
-
-		in.close();
-
-		String[][] returnArray = new String[result.size()][rowSize];
-
-		for (int i = 0; i < returnArray.length; i++) {
-
-			returnArray[i] = (String[]) result.get(i);
-
-		}
+	
+			returnArray = new String[result.size()][rowSize];
+	
+			for (int i = 0; i < returnArray.length; i++) {
+	
+				returnArray[i] = (String[]) result.get(i);
+	
+			}	
+		} finally {
+        	if (in != null) {
+        		in.close();
+        		in = null;
+        	}
+        }
 
 		return returnArray;
 
