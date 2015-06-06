6b9fdfef6d67ec96d680aa7a3d08407dd5c65c36
techpicker <1023144975@qq.com>
2015-06-06 21:24:13 +0800

    导入农行文件

---
+++ src/com/bank/beans/enums/FarmerNYEnum.java
@@ -0,0 +1,49 @@
+package com.bank.beans.enums;
+
+public enum FarmerNYEnum {
+	FARMERNAME(1),
+	IDTYPE(9),
+	IDNUM(11),
+	SEX(18),
+	FARMERTYPE(7),
+	MARRYSTATUS(20),
+	EDUCATION(21),
+	ADDRESS(26);
+	private int index;
+	public static final int $SEX = 1;
+	public static final int $EDUCATION = 2;
+	public static final int $MARRYSTATUS = 3;
+	FarmerNYEnum(int index){
+		this.index = index;
+	}
+	public int getIndex(){
+		return this.index;
+	};
+	public static int explain(int name,String value){
+		if(FarmerNYEnum.$SEX == name){
+			if("男".equals(value)){
+				return 1;
+			}else if("女".equals(value)){
+				return 2;
+			}else{
+				return 9;
+			}
+		}else if (FarmerNYEnum.$EDUCATION == name){
+			if("本科".equals(value)){
+				return 2;
+			}else{
+				return 9;
+			}
+		}else if(FarmerNYEnum.$MARRYSTATUS == name){
+			if("已婚".equals(value)){
+				return 1;
+			}else if("未婚".equals(value)){
+				return 3;
+			}else{
+				return 9;
+			}
+		}else{
+			return -99;
+		}
+	}
+}
---
+++ src/com/bank/beans/enums/LoanNYEnum.java
@@ -0,0 +1,55 @@
+package com.bank.beans.enums;
+
+public enum LoanNYEnum {
+
+	   CLIENTNUM(2),
+	   NOTENUM(12),
+	   COMPACTNUM(13),
+	   LOANDATE(10),
+	   LIMITDATE(19),
+	   CURRENCY(6),
+	   AMOUNT(9),
+	   BALANCE(11),
+	   CLIENTNAME(3),
+	   IDTYPE(15),
+	   IDNUM(16),
+	   GUARANTEETYPE(7),
+	   ORGANCODE(0),
+	   GUARANTEETYPE1(7);
+	
+	   private int index; 
+	   
+	   public static final int $CLIENTTYPE = 1;//客户类型
+	   public static final int $IDTYPE = 2;//证件类型
+	   
+	   LoanNYEnum(int index){
+		   this.index=index;
+	   }
+	   public int getIndex(){
+		   return this.index;
+	   }
+	   /**
+	    * 
+	    * @param type
+	    * @param value
+	    * @return -99翻译失败
+	    */
+	   public static Integer explain(int type,String value){
+		   if(LoanNSEnum.$CLIENTTYPE == type){
+			   if("其它自然人客户".equals(value)){
+				   return 1;
+			   }else{
+				   return -99;//翻译失败
+			   }
+		   }else if(LoanNSEnum.$IDTYPE == type){
+			   if("公民身份证".equals(value)){
+				   return 0;
+			   }else{
+				   return -99;//
+			   }
+			   
+		   }else{
+			   return -99;
+		   }
+	   }
+}
